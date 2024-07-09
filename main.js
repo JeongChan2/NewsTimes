a19ba97194783bb4b0965a3d442e1b40283f8dc6
JeongChan2 <jcstar55@gmail.com>
2024-07-09 16:09:00 +0900

    배포

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const API_KEY = `3643515049634840bf9e22e9ef948e16`
-const getLatestNews = () => {
+let news = [];
+const getLatestNews = async () => {
   const url = new URL(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   );
-  const response = fetch(url);
+  const response = await fetch(url);
+  const data = await response.json();
+  news = data.articles
+  console.log("rrrr", news);
 
 };
 
