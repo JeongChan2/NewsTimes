807d6ac0aca2b1c765c86bdb49db8a1cb24f2704
JeongChan2 <jcstar55@gmail.com>
2024-07-09 16:17:56 +0900

    Deploy

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const API_KEY = `3643515049634840bf9e22e9ef948e16`
+const DEPLOY_URL = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+const TEST_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
 let news = [];
 const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    `${DEPLOY_URL}`
   );
   const response = await fetch(url);
   const data = await response.json();
