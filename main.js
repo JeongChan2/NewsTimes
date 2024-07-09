fbb805b563f611fd6216d5519d6393aabc811ea5
JeongChan2 <jcstar55@gmail.com>
2024-07-09 21:58:15 +0900

    feat: 8

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-const API_KEY = `3643515049634840bf9e22e9ef948e16`
-const NUU_URL = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
-const DEPLOY_URL = `https://chan-news-times.netlify.app/top-headlines`
-const TEST_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+const API_KEY = `3643515049634840bf9e22e9ef948e16`;
+const NUU_URL = `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`;
+const DEPLOY_URL = `https://chan-news-times.netlify.app/top-headlines?`;
+const TEST_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+const X_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png';
 
+let RESULT_URL = DEPLOY_URL;
+let newsList = [];
 
-let news = [];
+const menuList1 = document.querySelectorAll(".side-menu-list button");
+const menuList2 = document.querySelectorAll(".menus button");
+const menuList = [...menuList1, ...menuList2];
+
+menuList.forEach((menuButton) => {
+  menuButton.addEventListener("click", () => {
+    let tmp = menuButton.textContent.toLowerCase();
+    // RESULT_URL = DEPLOY_URL + "category=Entertainment";
+    RESULT_URL = DEPLOY_URL + `category=${tmp}`;
+    console.log(RESULT_URL);
+    getLatestNews();
+  });
+});
 
 const openSearchBox = () => {
   let inputArea = document.getElementById("input-area");
@@ -24,20 +39,49 @@ const closeNav = () => {
 };
 
 const getLatestNews = async () => {
-  const url = new URL(
-    `${DEPLOY_URL}`
-  );
+  const url = new URL(`${RESULT_URL}`);
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles
-  console.log("rrrr", news);
+  newsList = data.articles;
+  console.log("rrrr", newsList);
 
-  news.forEach(article => {
+  render();
+};
+
+const render = () => {
+  let result = ``;
+  newsList.forEach((article) => {
+    let author = article.author;
     const urlToImage = article.urlToImage;
-    console.log("기사의 이미지 URL:", urlToImage);
-    // 여기서 urlImage를 사용하여 원하는 작업을 수행할 수 있습니다.
-  });
+    const title = article.title;
+    const description = article.description;
+    const publishedAt = article.publishedAt;
 
+    // console.log(moment(publishedAt).fromNow());
+
+    result += `<div class="row news">
+          <div class="col-lg-4 image-parent">
+            <img
+              class="news-img-size"
+              src="${urlToImage || X_IMAGE}" alt="이미지" onerror="this.onerror=null; this.src='${X_IMAGE}';"
+            />
+          </div>
+          <div class="col-lg-8">
+            <h2>${title}</h2>
+            <p>${
+              description == null || description == ""
+                ? "내용없음"
+                : description.length > 200
+                ? description.substring(0, 200) + "..."
+                : description
+            }</p>
+            <div>
+              ${author || "no source"}  ${moment(publishedAt).fromNow()}
+            </div>
+          </div>
+        </div>`;
+  }); // map으로 썼을땐 .join('')까지 해주기
+  document.getElementById("news-board").innerHTML = result;
 };
 
 getLatestNews();
